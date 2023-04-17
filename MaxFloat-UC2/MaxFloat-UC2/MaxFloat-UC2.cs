17d51b1bbb769f6e16c47cf36e0ad533649ec1ae
MSRbuddy <meghanadasari222@gmail.com>
2023-04-17 22:03:46 +0530

    Added : Find Maximum Float using Generics!

---
+++ MaxFloat-UC2/MaxFloat-UC2/MaxFloat-UC2.cs
@@ -0,0 +1,30 @@
+﻿namespace MaxFloat_UC2
+{
+    internal class Program
+    {
+        public static double MaxDouble(double firstvalue, double secondvalue, double thirdvalue)
+        {
+            firstvalue = 10.5;
+            secondvalue = 25.2;
+            thirdvalue = 3.1;
+
+            if (firstvalue.CompareTo(secondvalue) > 0 && firstvalue.CompareTo(thirdvalue) > 0)
+            {
+                return firstvalue;
+            }
+            if (secondvalue.CompareTo(firstvalue) > 0 && secondvalue.CompareTo(thirdvalue) > 0)
+            {
+                return secondvalue;
+            }
+            if (thirdvalue.CompareTo(firstvalue) > 0 && thirdvalue.CompareTo(secondvalue) > 0)
+            {
+                return thirdvalue;
+            }
+            return firstvalue;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MaxDouble(10.5, 25.2, 3.1));
+        }
+    }
+}
