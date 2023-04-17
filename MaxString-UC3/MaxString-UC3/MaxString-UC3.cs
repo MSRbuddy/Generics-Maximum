ad261e219c75164706dd762611f77858d2082de5
MSRbuddy <meghanadasari222@gmail.com>
2023-04-17 22:06:53 +0530

    Added : Find Maximum String using Generics!

---
+++ MaxString-UC3/MaxString-UC3/MaxString-UC3.cs
@@ -0,0 +1,30 @@
+﻿namespace MaxString_UC3
+{
+    internal class Program
+    {
+        public static string MaxString(string firstvalue, string secondvalue, string thirdvalue)
+        {
+            firstvalue = "Kanna";
+            secondvalue = "Anu";
+            thirdvalue = "Abhi";
+
+            if (firstvalue.CompareTo(secondvalue) > 0 && firstvalue.CompareTo(thirdvalue) > 0)
+            {
+                return firstvalue;
+            }
+            if (secondvalue.CompareTo(firstvalue) > 0 && secondvalue.CompareTo(thirdvalue) > 0)
+            {
+                return secondvalue;
+            }
+            if (thirdvalue.CompareTo(firstvalue) > 0 && thirdvalue.CompareTo(secondvalue) > 0)
+            {
+                return thirdvalue;
+            }
+            return firstvalue;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MaxString("Kanna", "Anu", "Abhi"));
+        }
+    }
+}
