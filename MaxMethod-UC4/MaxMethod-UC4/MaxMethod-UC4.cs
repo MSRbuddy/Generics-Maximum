a97fa8f36df5bcb62bb3194e2e9e7161485cb75b
MSRbuddy <meghanadasari222@gmail.com>
2023-04-17 22:15:14 +0530

    Added : Find Maximum using Generics-Method(UC-4)!

---
+++ MaxMethod-UC4/MaxMethod-UC4/MaxMethod-UC4.cs
@@ -0,0 +1,46 @@
+﻿namespace MaxMethod_UC4
+{
+    internal class Program
+    {
+        public static void toPrint(int[] Array)
+        {
+            foreach(int item in Array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        public static void toPrint(double[] Array)
+        {
+            foreach (double item in Array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        public static void toPrint(char[] Array)
+        {
+            foreach (char item in Array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        public static void toPrint(string[] Array)
+        {
+            foreach (string item in Array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[] intArray = {0, 5, 10, 15, 20, 25, 30};
+            double[] doubleArray = {1.2, 2.4, 3.6, 4.8, 5.9, 6.5, 7.8};
+            char[] charArray = {'M', 'E', 'G', 'H', 'A', 'N', 'A'};
+            string[] stringArray = {"One", "Two", "Three", "Four", "Five", "Six", "Seven"};
+
+            Program.toPrint(intArray);
+            Program.toPrint(doubleArray);
+            Program.toPrint(charArray);
+            Program.toPrint(stringArray);
+        }
+    }
+}
